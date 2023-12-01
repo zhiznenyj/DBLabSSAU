a26a1b5b4a9be3f6c29756f846e6675dd84a4b78
zhiznenyj <1xxx163xxx1@gmail.com>
2023-12-01 13:49:22 +0400

    fix 5-18

--- Lab #5/Task-5-18.sql
+++ Lab #5/Task-5-18.sql
@@ -1,39 +1,29 @@
--- Выводит скользящий средний общий доход за предыдущие 15 дней каждого дня августа 2012 года.
+-- Вывести скользящий средний общий доход за предыдущие 15 дней каждого дня августа 2012 года.
 SET @start_date = '2012-08-01', @end_date = '2012-08-31';
-WITH
-    RECURSIVE dArray(dateDay) AS (
-        SELECT @start_date as dateDay
-        UNION ALL
-        SELECT DATE_ADD(dateDay, INTERVAL 1 day) FROM dArray WHERE dateDay < @end_date
-    ),
-    rolling_income AS (
-        SELECT
-            DATE_FORMAT(book.starttime, '%m %d') as dayMonth,
-            SUM(IF(book.memid = 0, fac.guestcost, fac.membercost) * book.slots) AS income,
-            ROUND(
-                AVG(
-                    SUM(
-                        IF(
-                            book.memid = 0,
-                            fac.guestcost,
-                            fac.membercost
-                        ) * book.slots
-                    )
-                ) OVER win
-            ) as roll_avg
-        FROM cd.bookings as book
-        LEFT JOIN cd.facilities as fac ON book.facid = fac.facid
-        WHERE MONTH(book.starttime) IN (7, 8)
-        GROUP BY dayMonth
-        WINDOW win as (
-            ORDER BY DATE_FORMAT(book.starttime, '%m %d') ROWS BETWEEN 14 PRECEDING AND CURRENT ROW 
-        )
-        ORDER BY dayMonth
-    )
+WITH RECURSIVE dArray(dateDay) AS (
+    SELECT @start_date as dateDay
+    UNION ALL
+    SELECT DATE_ADD(dateDay, INTERVAL 1 day)
+    FROM dArray
+    WHERE dateDay < @end_date
+)
 SELECT
-    dayMonth,
-    IFNULL(income, 0) as income,
-    IFNULL(roll_avg, 0) as roll_avg
-FROM dArray as DA
-LEFT JOIN rolling_income as RI ON RI.dayMonth = DATE_FORMAT(DA.dateDay, '%m %d')
-WHERE SUBSTRING(dayMonth, 1, 2) = '08';
+    dateDay
+,   IFNULL(SUM(
+        CASE
+            WHEN (DATE(book.starttime) != dateDay) THEN 0
+            WHEN (book.memid = 0) THEN fac.guestcost * book.slots
+            ELSE fac.membercost * book.slots
+        END
+    ), 0) as income
+,   IFNULL(ROUND(SUM(
+        IF(
+            book.memid = 0,
+            fac.guestcost,
+            fac.membercost
+        ) * book.slots
+    ) / 15), 0) as avg_roll
+FROM dArray
+LEFT JOIN cd.bookings as book ON DATE_ADD(dateDay, INTERVAL -14 day) <= DATE(book.starttime) AND DATE(book.starttime) <= dateDay
+LEFT JOIN cd.facilities as fac ON book.facid = fac.facid
+GROUP BY dateDay;
